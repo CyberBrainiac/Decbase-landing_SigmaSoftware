2c5ef7578780b64bc89347953dbd34d72f32c86d
Arseniy Arkhipov <arsenij.arxipov.1998@gmail.com>
2023-07-24 19:37:49 +0300

    firs step with loader

---
+++ common.blocks/overlay/overlay.js
@@ -0,0 +1,9 @@
+"use strict"
+createOverlay();
+
+function createOverlay() {
+	/**Create and style Overlay*/
+	let overlay = document.createElement("div");
+	overlay.classList.add('.overlay');
+	debugger;
+}
